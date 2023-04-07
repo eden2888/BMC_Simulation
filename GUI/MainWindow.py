83e311b51281f74af9bc87cb63f2b3e2b32a1600
eden2888 <edeneden288@gmail.com>
2023-04-07 20:50:05 +0300

    Added UI buttons link to logic methods

--- GUI/MainWindow.py
+++ GUI/MainWindow.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QPushButton, QVBo
 from PyQt5 import uic
 import sys
 
+
 class UI(QMainWindow):
     def __init__(self):
         super(UI, self).__init__()
@@ -9,9 +10,20 @@ class UI(QMainWindow):
         #load ui file:
         uic.loadUi("MainUI.ui", self)
 
+        #link button clicks to relevant methods:
+        self.BtnGenerator.clicked.connect(self.generatorButtonClicked)
+        self.BtnTester.clicked.connect(self.testerButtonClicked)
+        self.BtnVisual.clicked.connect(self.vusializeButtonClicked)
+
         # Show the app
         self.show()
 
+    def generatorButtonClicked(self):
+        print('generator button clicked')
+    def testerButtonClicked(self):
+        print('tester button clicked')
+    def vusializeButtonClicked(self):
+        print('visualize button clicked')
 
 # Initializing the app
 app = QApplication(sys.argv)
