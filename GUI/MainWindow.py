bfbe450cfa957e334fdec684f5a20e8ee42b676c
eden2888 <edeneden288@gmail.com>
2023-04-08 13:05:11 +0300

    Added 2nd screen with more data

--- GUI/MainWindow.py
+++ GUI/MainWindow.py
@@ -1,16 +1,35 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QPushButton, QVBoxLayout, QMenuBar, QStatusBar
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QPushButton, QVBoxLayout, QMenuBar, QStatusBar, QWidget
 from PyQt5 import uic
 import sys
+
+generatorWidget = None
+
+
+class GeneratorUI(QWidget):
+    def __init__(self, mainWidget):
+        super(GeneratorUI, self).__init__()
+        # load ui file:
+        uic.loadUi("GeneratorUI.ui", self)
+
+        # connect back button logic:
+        self.BtnBack.clicked.connect(self.backToMain)
+
+    def backToMain(self):
+        self.setCentralWidget(self.mainWidget)
 
 
 class UI(QMainWindow):
     def __init__(self):
         super(UI, self).__init__()
 
-        #load ui file:
+        # load ui file:
+        self.mainWidget = None
         uic.loadUi("MainUI.ui", self)
 
-        #link button clicks to relevant methods:
+        # load generator widget:
+        self.mainWidget = self.centralWidget()
+        self.generatorWidget = GeneratorUI(self.mainWidget)
+        # link button clicks to relevant methods:
         self.BtnGenerator.clicked.connect(self.generatorButtonClicked)
         self.BtnTester.clicked.connect(self.testerButtonClicked)
         self.BtnVisual.clicked.connect(self.vusializeButtonClicked)
@@ -20,10 +39,15 @@ class UI(QMainWindow):
 
     def generatorButtonClicked(self):
         print('generator button clicked')
+        self.mainWidget = self.takeCentralWidget()
+        self.setCentralWidget(self.generatorWidget)
+
     def testerButtonClicked(self):
         print('tester button clicked')
+
     def vusializeButtonClicked(self):
         print('visualize button clicked')
+
 
 # Initializing the app
 app = QApplication(sys.argv)
