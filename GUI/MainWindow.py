30c5c5ea9c51aeaa9ca3faae319262673e128270
eden2888 <edeneden288@gmail.com>
2023-04-08 13:58:25 +0300

    Fixed GUI architecture and added generator ui widget

--- GUI/MainWindow.py
+++ GUI/MainWindow.py
@@ -1,46 +1,36 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QPushButton, QVBoxLayout, QMenuBar, QStatusBar, QWidget
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QPushButton, QVBoxLayout, QMenuBar, QStatusBar, QWidget, \
+    QStackedWidget, QSizePolicy
 from PyQt5 import uic
 import sys
 
-generatorWidget = None
 
-
-class GeneratorUI(QWidget):
-    def __init__(self, mainWidget):
-        super(GeneratorUI, self).__init__()
+class GeneratorWidget(QWidget):
+    def __init__(self, parent=None):
+        super(GeneratorWidget, self).__init__(parent)
         # load ui file:
         uic.loadUi("GeneratorUI.ui", self)
 
-        # connect back button logic:
-        self.BtnBack.clicked.connect(self.backToMain)
-
     def backToMain(self):
-        self.setCentralWidget(self.mainWidget)
+        print('back button pressed')
 
 
-class UI(QMainWindow):
-    def __init__(self):
-        super(UI, self).__init__()
+class StartWidget(QWidget):
+
+    def __init__(self, parent=None):
+        super(StartWidget, self).__init__(parent)
+        # super(StartWidget, self).__init__()
 
         # load ui file:
-        self.mainWidget = None
-        uic.loadUi("MainUI.ui", self)
+        uic.loadUi("StartUI.ui", self)
 
-        # load generator widget:
-        self.mainWidget = self.centralWidget()
-        self.generatorWidget = GeneratorUI(self.mainWidget)
         # link button clicks to relevant methods:
-        self.BtnGenerator.clicked.connect(self.generatorButtonClicked)
-        self.BtnTester.clicked.connect(self.testerButtonClicked)
-        self.BtnVisual.clicked.connect(self.vusializeButtonClicked)
-
-        # Show the app
-        self.show()
+        # self.BtnGenerator.clicked.connect(self.generatorButtonClicked)
+        # self.BtnTester.clicked.connect(self.testerButtonClicked)
+        # self.BtnVisual.clicked.connect(self.vusializeButtonClicked)
 
     def generatorButtonClicked(self):
         print('generator button clicked')
-        self.mainWidget = self.takeCentralWidget()
-        self.setCentralWidget(self.generatorWidget)
 
     def testerButtonClicked(self):
         print('tester button clicked')
@@ -49,7 +39,36 @@ class UI(QMainWindow):
         print('visualize button clicked')
 
 
+class UI(QMainWindow):
+    def __init__(self, parent=None):
+        super(UI, self).__init__()
+        self.central_widget = QStackedWidget()
+        self.setCentralWidget(self.central_widget)
+        # set up secondary widgets
+        self.start_screen = StartWidget(self)
+        self.generator_screen = GeneratorWidget(self)
+        # add widgets to stack widget:
+        self.central_widget.addWidget(self.start_screen)
+        self.central_widget.addWidget(self.generator_screen)
+        self.central_widget.setCurrentWidget(self.start_screen)
+        # fix central widget visuals:
+        self.setStyleSheet("background-color: rgb(160, 210, 235)")
+
+        # setup signal connections:
+        self.start_screen.BtnGenerator.clicked.connect(self.setGeneratorScreen)
+        self.generator_screen.BtnBack.clicked.connect(self.setStartScreen)
+
+    def setGeneratorScreen(self):
+        self.central_widget.setCurrentWidget(self.generator_screen)
+
+    def setStartScreen(self):
+        self.central_widget.setCurrentWidget(self.start_screen)
+
+
 # Initializing the app
 app = QApplication(sys.argv)
 UIWindow = UI()
+# 670,150 = x,y for startup location, 800x500 = app resolution
+UIWindow.setGeometry(670, 150, 800, 500)
+UIWindow.show()
 app.exec_()
